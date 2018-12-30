2e585ebff96abacaf57ce4f9e24da9fb2e55abbc
GeonLee <leegun2003@gmail.com>
2018-12-30 20:27:10 +0900

    API Verbs

---
+++ README.md
@@ -0,0 +1,21 @@
+# Nomflix
+
+Learning React and ES6 by building a Movie Discovery App.
+
+## Screens
+
+- [ ] Home
+- [ ] TV Shows
+- [ ] Search
+- [ ] Detail
+
+## API Verbs
+
+- [x] Now playing (Movie)
+- [x] Upcoming (Movie)
+- [x] Top Rated (TV)
+- [x] Popular (TV, X)
+- [x] Airing Today (TV)
+- [x] TV Show Detail
+- [x] Movie Detail
+- [x] Search (Movie, TV)
---
+++ src/Components/Router.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
+import Home from "Routes/Home";
+import TV from "Routes/TV";
+import Header from "Components/Header";
+import Search from "Routes/Search";
+
+export default () => (
+  <Router>
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" component={TV} />
+        <Route path="/search" component={Search} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
+  </Router>
+);
