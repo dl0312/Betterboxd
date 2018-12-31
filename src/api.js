cfbe38110cafd5149db9c9f878542d0adf1e1a4f
GeonLee <leegun2003@gmail.com>
2018-12-31 16:23:06 +0900

    HomeContainer

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+require("dotenv").config();
 
 const api = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   params: {
-    api_key: process.env.API_KEY,
+    api_key: process.env.REACT_APP_API_KEY,
     language: "ko-KR"
   }
 });
